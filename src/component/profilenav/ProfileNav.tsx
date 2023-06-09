5788232ef11334e540b66771201e73c4b5bd2b6f
AdarshVMore <skullcrushermore@gmail.com>
2023-06-09 17:47:27 +0530

    profile navbar done

--- src/component/profilenav/ProfileNav.tsx
+++ src/component/profilenav/ProfileNav.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import "./profilenav.css";
 
 function ProfileNav() {
-  return <div>ProfileNav</div>;
+  return (
+    <div className="profile-nav">
+      <button className="post-design-btn">Post Design</button>
+      <button className="start-streaming-btn">Start Streaming</button>
+    </div>
+  );
 }
 
 export default ProfileNav;
