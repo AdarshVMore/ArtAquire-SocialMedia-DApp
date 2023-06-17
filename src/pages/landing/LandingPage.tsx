38cbd9525d605910e000fd69b41ce6208b18c739
Srikar Mk <srikarmks@gmail.com>
2023-06-17 13:50:19 +0530

    Landing page done

--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,8 +1,34 @@
 import React from "react";
 import "./landingps.css";
-
+import landing from "./landing-img.svg"
+import logo from "./logo.svg"
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return <div>
+    <nav>
+      <div className="landing-logo"><img src={logo} alt="logo"></img></div>
+      <div>
+        <ul className="nav-list">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Features</a></li>
+          <li><a href="#">Working</a></li>
+          <li><a href="#">About</a></li>
+          <li><button className="nav-launch">Launch</button></li>
+        </ul>
+      </div>
+    </nav>
+    <section className="landing-main">
+      <div className="content">
+        <h1>ArtAquire</h1>
+        <h3>Unlock Your Artistic Journey in the Web3 Metaverse</h3>
+        <p>Join the Web3 Art Revolution Today! Be part of a thriving community, showcase your artwork, sell securely, and connect with like-minded artists.</p>
+        <button className="launch">Launch Dapp</button>
+      </div>
+      <div className="landing-img">
+        <img src={landing} alt="graphic-image"/>
+      </div>
+
+    </section>
+  </div>;
 }
 
 export default LandingPage;
